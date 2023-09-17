2f48e3b7d4a111c055e76b43f15190a730fdbf8d
BitWarlock <43728556+BitWarlock@users.noreply.github.com>
2023-09-17 15:38:40 +0100

    betty fix

--- _printf.c
+++ _printf.c
@@ -31,7 +31,7 @@ int	_printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-	if (format[0] == '%' && format[1])
+	if (format[0] == '%' && format[1] == '\0')
 		return (-1);
 	va_start(args, format);
 	while (*format)
