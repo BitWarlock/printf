9af8363eed7f612c38760144624a4da88787dfd9
uuddffee <122704559+uuddffee@users.noreply.github.com>
2023-09-17 13:41:33 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/* libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+void _putchar(char c);
+
+#endif
